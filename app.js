9ded3110a965d44cc71ff07881de992689e8d730
larsyaeger <larsyaeger@gmail.com>
2022-12-28 11:34:54 -0600

    begin adding questions objects

---
+++ app.js
@@ -0,0 +1,23 @@
+const questionDisplay = document.querySelector('#questions');
+const answerDisplay = document.querySelector('#answer');
+
+const questions = [
+  {
+    id: 0,
+    text: 'Pick a vacation destination',
+    answers: [
+      {
+        text: 'New York',
+        image: 'https://images.unsplash.com/photo-1555109307-f7d9da25c244?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2673&q=80',
+        alt: 'Photo of the Empire State building',
+        credit: 'Kit Suman'
+      }
+    ]
+  },
+  {
+
+  },
+  {
+
+  }
+]
